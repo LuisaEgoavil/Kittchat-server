071bb760520a35a4afa0254568c15edd55c50cc7
Luisa Egoavil <lupi@Luisas-Air.fritz.box>
2021-03-06 17:23:45 +0100

    commit

--- seeds.js
+++ seeds.js
@@ -4,11 +4,11 @@ const LocationModel = require("./models/Location.model.js")
 
 // locations to be added
 let locations = [
-  { cafeName: "Kittchat", address: "Manteuffelstraße 20, 12103 Berlin", hours: "11:00-19:00", phoneNumber: 017695853433, email: "kittchatcafe@gmail.com"},
-  { cafeName: "Kittchat", address: "Otto-Suhr-Allee 12, 10585 Berlin", hours: "11:30-19:00", phoneNumber: 056195015988, email: "kittchatcafe@gmail.com"},
-  { cafeName: "Kittchat", address: "Paul-Lincke-Ufer 25A, 10999 Berlin", hours: "11:00-20:00", phoneNumber: 051598952555, email: "kittchatcafe@gmail.com"},
-  { cafeName: "Kittchat", address: "Leykestraße 8A, 12053 Berlin", hours: "12:00-19:30", phoneNumber: 017691442255, email: "kittchatcafe@gmail.com"},
-  { cafeName: "Kittchat", address: "Marienburger Straße 27, 10405 Berlin", hours: "11:00-20:00", phoneNumber: 015962582122, email: "kittchatcafe@gmail.com"}
+  { cafeName: "Kittchat Schöneberg", address: "Manteuffelstraße 20, 12103 Berlin", hours: "11:00-19:00", phoneNumber: 017695853433, email: "kittchatcafe@gmail.com"},
+  { cafeName: "Kittchat Charlottenburg", address: "Otto-Suhr-Allee 12, 10585 Berlin", hours: "11:30-19:00", phoneNumber: 056195015988, email: "kittchatcafe@gmail.com"},
+  { cafeName: "Kittchat Kreuzberg", address: "Paul-Lincke-Ufer 25A, 10999 Berlin", hours: "11:00-20:00", phoneNumber: 051598952555, email: "kittchatcafe@gmail.com"},
+  { cafeName: "Kittchat Neukölln", address: "Leykestraße 8A, 12053 Berlin", hours: "12:00-19:30", phoneNumber: 017691442255, email: "kittchatcafe@gmail.com"},
+  { cafeName: "Kittchat Prenzlauer Berg", address: "Marienburger Straße 27, 10405 Berlin", hours: "11:00-20:00", phoneNumber: 015962582122, email: "kittchatcafe@gmail.com"}
 ]
 
 //to add an array of several elemnts to DB
