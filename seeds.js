b743ef9de88cd472432d9ac06da531593c9f7147
Megumikawa <megumi19901016@gmail.com>
2021-03-06 18:00:53 +0100

    chg location name

--- seeds.js
+++ seeds.js
@@ -4,11 +4,11 @@ const LocationModel = require("./models/Location.model.js")
 
 // locations to be added
 let locations = [
-  { cafeName: "locationA", address: "Germany", hours: "11:00-18:00", phoneNumber: 1111111, email: "aaa@aaa.com"},
-  { cafeName: "locationB", address: "Spain", hours: "11:00-19:00", phoneNumber: 2222222, email: "bbb@aaa.com"},
-  { cafeName: "locationC", address: "England", hours: "11:00-20:00", phoneNumber: 3333333, email: "ccc@aaa.com"},
-  { cafeName: "locationD", address: "Finland", hours: "11:00-21:00", phoneNumber: 4444444, email: "ddd@aaa.com"},
-  { cafeName: "locationE", address: "France", hours: "11:00-22:00", phoneNumber: 5555555, email: "eee@aaa.com"}
+  { cafeName: "location1", address: "Germany", hours: "11:00-18:00", phoneNumber: 1111111, email: "aaa@aaa.com"},
+  { cafeName: "location2", address: "Spain", hours: "11:00-19:00", phoneNumber: 2222222, email: "bbb@aaa.com"},
+  { cafeName: "location3", address: "England", hours: "11:00-20:00", phoneNumber: 3333333, email: "ccc@aaa.com"},
+  { cafeName: "location4", address: "Finland", hours: "11:00-21:00", phoneNumber: 4444444, email: "ddd@aaa.com"},
+  { cafeName: "location5", address: "France", hours: "11:00-22:00", phoneNumber: 5555555, email: "eee@aaa.com"}
 ]
 
 //to add an array of several elemnts to DB
